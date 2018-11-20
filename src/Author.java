48b2b3b410655c81f4411104d09506fdf9f65822
Stuart Hamblin <stuart@Stuarts-MacBook-Air.local>
2018-11-20 11:04:55 -0600

    added String type to List books and the arraylist in with the auther constructor

--- src/Author.java
+++ src/Author.java
@@ -4,13 +4,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("unchecked")
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<String>();
     }
 
     /**
@@ -28,7 +27,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    public String sortName() {                                                          //gives an error if @Override
+    public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
