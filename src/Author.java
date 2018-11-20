f22de76d7061f3e3179e44a2d02393c407ad2ad6
Stuart Hamblin <stuart@Stuarts-MacBook-Air.local>
2018-11-20 10:15:28 -0600

    commented out @Override b/c it produces an error

--- src/Author.java
+++ src/Author.java
@@ -28,7 +28,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+//    @Override                                                                    //gives an error if not commented out
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
